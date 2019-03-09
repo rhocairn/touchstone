200bda6faa0c6dbd32fc719e94db82f85368c253
Gabe Maybrun <gmaybrun@gmail.com>
2019-03-08 18:53:20 -0800

    Release script also tags in git now

--- scripts/upload-dist.sh
+++ scripts/upload-dist.sh
@@ -2,7 +2,13 @@
 
 set -euxo pipefail
 
+VERSION="$(python setup.py --version)"
+TAG="v${VERSION}"
+
 rm -rf dist/
 python setup.py sdist bdist_wheel
 twine check dist/*
 python -m twine upload dist/*
+
+git tag -a "${TAG}" -m "Release v${VERSION}"
+git push origin "${TAG}"
